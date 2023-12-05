850cc992c4303f6328a0db09027388293807235d
devn0n <sochi98.sn@gmail.com>
2023-12-05 08:41:53 +0100

    Initial commit - Collect user input and get rand crate

---
+++ src/main_.rs
@@ -0,0 +1,13 @@
+// use std::io;
+
+fn main() {
+    println!("Bla");
+
+    println!("Bla");
+
+    let data = 10;
+
+    // io::stdin().read_line(&mut data).expect("Error");
+
+    println!("Guessed {}", data + 2);
+}
